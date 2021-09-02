83505aed19b3aa9aca34651903bd1e73d5dfc948
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-02 21:48:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Yandex-Image-Search-Bot/blob/main/LICENSE
